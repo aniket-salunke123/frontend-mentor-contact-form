cc4aba664423a0bab8c039e238dc5d25ece5a0a3
aniket salunke <aniket3984@gmail.com>
2024-09-29 00:26:01 +0530

    added angular form controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
@@ -18,9 +21,25 @@ import {MatButtonModule} from '@angular/material/button';
     MatIconModule,
     MatRadioModule,
     MatCheckboxModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  contactForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    
+    this.contactForm = this.formBuilder.group({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      queryType: new FormControl('', [Validators.required]),
+      message: new FormControl('', [Validators.required]),
+      consent: new FormControl(false, [Validators.requiredTrue])
+    });
+  }
+
+}
